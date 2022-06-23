f1d1c8295a8cb2e269be4c6f0ceafeaf1eda1210
jinseo08 <kjs_8086@naver.com>
2022-06-24 08:33:38 +0900

    인프런 스프링부트 강의

---
+++ src/test/java/com/example/inflearnProject/InflearnProjectApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.inflearnProject;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class InflearnProjectApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
